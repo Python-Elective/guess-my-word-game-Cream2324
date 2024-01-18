05f2307c6d975d2043a840a3d82b8696fa2523bf
Cream2324 <143768232+Cream2324@users.noreply.github.com>
2024-01-18 09:49:50 +0700

    commit

--- gusse my word game _Cream_1101.py
+++ gusse my word game _Cream_1101.py
@@ -1,34 +1,30 @@
-
-def is_word_guessed(secret_word, letters_guessed):
-    '''
-    secret_word: string, the word the user is guessing
-    letters_guessed: list, what letters have been guessed so far
-    returns: boolean, True if all the letters of secret_word are in letters_guessed;
-      False otherwise
-    '''
-    """
-    for every letter in secret_word
-        if the letter is not in letter_guessed
-            stop looking and return false
-    return true
-    """
+# # Problem 1
+# def is_word_guessed(secret_word, letters_guessed):
+#     '''
+#     secret_word: string, the word the user is guessing
+#     letters_guessed: list, what letters have been guessed so far
+#     returns: boolean, True if all the letters of secret_word are in letters_guessed;
+#       False otherwise
+#     '''
+#     """
+#     for every letter in secret_word
+#         if the letter is not in letter_guessed
+#             stop looking and return false
+#     return true
+#     """
  
-    for letter in secret_word:
-        if letter not in letters_guessed:
-            return False
-    return True
+#     for letter in secret_word:
+#         if letter not in letters_guessed:
+#             return False
+#     return True
        
  
-### Testcases
-print(is_word_guessed('apple', ['a', 'e', 'i', 'k', 'p', 'r', 's']))
-print(is_word_guessed('mangosteen', ['z', 'x', 'q', 'm', 'a', 'n', 'g', 'o', 's', 't', 'e', 'e', 'n']))
-print(is_word_guessed('pineapple', []))
+# ### Testcases
+# print(is_word_guessed('apple', ['a', 'e', 'i', 'k', 'p', 'r', 's']))
+# print(is_word_guessed('mangosteen', ['z', 'x', 'q', 'm', 'a', 'n', 'g', 'o', 's', 't', 'e', 'e', 'n']))
+# print(is_word_guessed('pineapple', []))
  
- 
- 
- 
- 
- 
+# # Problem 2
 # def get_guessed_word(secret_word, letters_guessed):
 #     '''
 #     secret_word: string, the word the user is guessing
@@ -44,59 +40,43 @@ print(is_word_guessed('pineapple', []))
 #             output_string += "_ "
 #     return output_string
  
-# print(get_guessed_word('apple', ['a', 'e', 'i', 'k', 'p', 'r', 's']))
+# print(get_guessed_word('apple', ['e', 'i', 'k', 'p', 'r', 's']))
 # print(get_guessed_word('durian', ['h', 'a', 'c', 'd', 'i', 'm', 'n', 'r', 't', 'u']))
-# print(get_guessed_word('pineapple', []))
+# print(get_guessed_word ('banana', []))
  
-# def get_available_letters(letters_guessed):
-#     '''
-#     letters_guessed: list, what letters have been guessed so far
-#     returns: string, comprised of letters that represents what letters have not
-#       yet been guessed.
-#     '''  
-#     import string
-#     alphabet = list(string.ascii_lowercase)
+
+# Problem 3
+def get_available_letters(letters_guessed):
+    '''
+    letters_guessed: list, what letters have been guessed so far
+    returns: string, comprised of letters that represents what letters have not
+      yet been guessed.
+    '''  
+    import string
+    alphabet = list(string.ascii_lowercase)
  
-#     for letter in alphabet:
-#         if letter in letters_guessed:
-#             alphabet.remove(letter)
+    for letter in alphabet:
+        if letter in letters_guessed:
+            alphabet.remove(letter)
  
-#     return ' '.join(alphabet)
+    return ' '.join(alphabet)
    
-# print(get_available_letters('apple'))
-# print(get_available_letters(''))
+print(get_available_letters('apple'))
+print(get_available_letters(''))
 
+# 4
+while True: #game loop
+# while player != Dead or point >0 ........
+    if player == detaile
+    You diee
+    break #game over
 
-#     # FILL IN YOUR CODE HERE...
+    if point ==0
+    You Loooose
+    break # game over
 
+    if guess_word == True 
+    You WIN break # game over
 
-#     #option 4
-#     # out put_ strin =
-#     # for every letter in alpjabet
-#     # if letter not in letter_guessed 
-#     # letter is still availbele
-#     # concatenate letter onto output
-#     # otherwisw 
-#     # do nothing
-#     #return output_string
-
-# #testcase
-# print( get _avilable_letter({'e', 'o', }))
-#     print(get_available_letters)
-
-# get game loop(secret_word)
-
-
-# # 4
-# while True: #game loop
-# # while player != Dead or point >0 ........
-#     if player == detaile
-#     You diee
-#     break #game over
-
-# if point ==0
-# You Loooose
-# break # game over
-
-# if guess_word == True 
-# You WIN break # game over
+sedef main():
+secret_word = choose_word(word_list)
