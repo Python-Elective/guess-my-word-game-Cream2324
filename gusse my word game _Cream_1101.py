123536662dd4e3ca6bb1144b72d11056a1846f71
Cream2324 <143768232+Cream2324@users.noreply.github.com>
2024-02-02 09:27:12 +0700

    commit

--- gusse my word game _Cream_1101.py
+++ gusse my word game _Cream_1101.py
@@ -1,3 +1,47 @@
+#  -----------------------------------
+# Helper code
+# You don't need to understand this helper code,
+# but you will have to know how to use the functions
+# (so be sure to read the docstrings!)
+
+import random
+import string
+
+WORDLIST_FILENAME = "word_list.txt"
+
+def load_words():
+    """
+    Returns a list of valid words. Words are strings of lowercase letters.
+    
+    Depending on the size of the word list, this function may
+    take a while to finish.
+    """
+    print("Reading word_list file...")
+    # inFile: file
+    inFile = open(WORDLIST_FILENAME, 'r')
+    # line: string
+    line = inFile.readline()
+    # word_list: list of strings
+    word_list = line.split()
+    print(len(word_list), "words found")
+    return word_list
+
+def choose_word(word_list):
+    """
+    word_list (list): list of words (strings)
+
+    Returns a word from word_list at random
+    """
+    return random.choice(word_list)
+
+# end of helper code
+# -----------------------------------
+
+# Load the list of words into the variable word_list
+# so that it can be accessed from anywhere in the program
+word_list = load_words()
+
+
 # Problem 1
 def is_word_guessed(secret_word, letters_guessed):
     '''
@@ -113,7 +157,7 @@ def game_loop(secret_word):
             break  # game stops
 
 def main():
-    word_list = ["apple", "banana", "cherry"]  # Add more words as needed
+    # word_list = ["apple", "banana", "cherry"]  # Add more words as needed
     secret_word = choose_word(word_list)
     game_loop(secret_word)
 
